6ecbe52884e2ce5f7d93bc67e6d77acb4b4f1f77
J-Cisne <jasmine.cisneros01@sjsu.edu>
2026-02-10 22:37:11 -0800

    commit 3 files

--- sparkle.js
+++
@@ -1,7 +0,0 @@
-// Play sparkle sound on click
-const sparkleAudio = new Audio('sparkle.mp3');
-
-document.addEventListener('click', () => {
-  sparkleAudio.currentTime = 0;
-  sparkleAudio.play();
-});
